2b54676f59d109c3ab199e8b5e3f7e6854afe505
Neemias Santos <neemiassgc@outlook.com>
2023-12-09 01:32:24 -0300

    new: Create a repository class for the Contact entity named ContactRepository

---
+++ src/main/java/com/spring/boot/repositories/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.spring.boot.repositories;
+
+import com.spring.boot.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
