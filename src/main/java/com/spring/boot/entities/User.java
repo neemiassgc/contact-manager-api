a4e8b3bada272fd9192549b74f8b5e322378c1f4
Neemias Santos <neemiassgc@outlook.com>
2023-12-09 01:06:00 -0300

    new: Create and map a new entity named User

---
+++ src/main/java/com/spring/boot/entities/User.java
@@ -0,0 +1,30 @@
+package com.spring.boot.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @UuidGenerator
+    private UUID id;
+
+    @Column(nullable = false, length = 20, unique = true)
+    @Setter
+    private String username;
+
+    public User(final String username) {
+        this.username = username;
+    }
+}
