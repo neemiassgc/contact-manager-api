d81cf2436c6b5f3e6b229dabd801d2e0d6ada896
Neemias Santos <neemiassgc@outlook.com>
2023-12-08 23:39:05 -0300

    refactor: Add modifications on the constraints of the tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,34 +6,43 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS contacts (
     contact_id UUID PRIMARY KEY,
     name VARCHAR(140) NOT NULL,
-    type VARCHAR(15) NOT NULL,
-    user_id UUID NOT NULL UNIQUE,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    user_id UUID NOT NULL,
+    CONSTRAINT fk_users
+        FOREIGN KEY (user_id)
+        REFERENCES users (user_id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS emails (
-    email_id UUID PRIMARY KEY,
     type VARCHAR(15) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    contact_id UUID NOT NULL UNIQUE,
-    FOREIGN KEY (contact_id) REFERENCES contacts (contact_id)
+    contact_id UUID NOT NULL,
+    CONSTRAINT fk_contacts
+        FOREIGN KEY (contact_id)
+        REFERENCES contacts (contact_id),
+    PRIMARY KEY (type, contact_id)
 );
 
 CREATE TABLE IF NOT EXISTS phone_numbers (
-    phone_number_id UUID PRIMARY KEY,
     type VARCHAR(15) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    contact_id UUID NOT NULL UNIQUE,
-    FOREIGN KEY (contact_id) REFERENCES contacts (contact_id)
+    contact_id UUID NOT NULL,
+    CONSTRAINT fk_contacts
+        FOREIGN KEY (contact_id)
+        REFERENCES contacts (contact_id),
+    PRIMARY KEY (type, contact_id)
 );
 
-CREATE TABLE IF NOT EXISTS address (
-    address_id UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS addresses (
+    type VARCHAR(15) NOT NULL,
     country VARCHAR(20) NOT NULL,
     street VARCHAR(50) NOT NULL,
     city VARCHAR(50) NOT NULL,
     state VARCHAR(50) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL,
-    contact_id UUID NOT NULL UNIQUE,
-    FOREIGN KEY (contact_id) REFERENCES contacts (contact_id)
+    zipcode VARCHAR(20) NOT NULL,
+    contact_id UUID NOT NULL,
+    CONSTRAINT fk_contacts
+        FOREIGN KEY (contact_id)
+        REFERENCES contacts (contact_id),
+    PRIMARY KEY (type, contact_id)
 );
