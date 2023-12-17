942d78d98d880e35da84f889edac944cc287cadb
Neemias Santos <neemiassgc@outlook.com>
2023-12-17 09:38:02 -0300

    refactor: Fix a little bug that involve using Long instead of UUID as identifier

--- src/main/java/com/spring/boot/repositories/ContactRepository.java
+++ src/main/java/com/spring/boot/repositories/ContactRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> {
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
 
     @Query("select c, p, e, a from Contact c join fetch c.phoneNumberMap p join fetch c.emailMap e join fetch addressMap a")
     List<Contact> getAll();
