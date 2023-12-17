92dd6184123c1992287f9acd7b21e76f5e87dfce
Neemias Santos <neemiassgc@outlook.com>
2023-12-17 01:00:54 -0300

    new: Add a query method with custom JPQL named getAll

--- src/main/java/com/spring/boot/repositories/ContactRepository.java
+++ src/main/java/com/spring/boot/repositories/ContactRepository.java
@@ -2,8 +2,14 @@ package com.spring.boot.repositories;
 
 import com.spring.boot.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    @Query("select c, p, e, a from Contact c join fetch c.phoneNumberMap p join fetch c.emailMap e join fetch addressMap a")
+    List<Contact> getAll();
 }
