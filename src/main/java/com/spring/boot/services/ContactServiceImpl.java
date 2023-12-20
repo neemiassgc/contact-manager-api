73bd644aaaf5af0ebb59561a405b2355b4ca90e6
Neemias Santos <neemiassgc@outlook.com>
2023-12-19 23:42:24 -0300

    new: Implement getById query method from the contract interface ContactService

--- src/main/java/com/spring/boot/services/ContactServiceImpl.java
+++ src/main/java/com/spring/boot/services/ContactServiceImpl.java
@@ -4,25 +4,35 @@ import com.spring.boot.entities.Contact;
 import com.spring.boot.entities.projections.ContactSummary;
 import com.spring.boot.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
 
     @Override
-    @Transactional
     public List<ContactSummary> getAll() {
         return contactRepository
             .getAll()
             .stream()
             .map(Contact::toContactSummary)
             .collect(Collectors.toList());
+    }
+
+    @Override
+    public ContactSummary getById(UUID id) {
+       return contactRepository.findById(id)
+           .map(Contact::toContactSummary)
+           .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "contact not found"));
     }
 }
