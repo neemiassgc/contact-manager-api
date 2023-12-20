eca230f95b43045b794eb05332bf7e1251e1d8f0
Neemias Santos <neemiassgc@outlook.com>
2023-12-19 23:40:11 -0300

    new: Add four more storage service methods which are: getById, save, update and deleteById

--- src/main/java/com/spring/boot/services/ContactService.java
+++ src/main/java/com/spring/boot/services/ContactService.java
@@ -1,10 +1,20 @@
 package com.spring.boot.services;
 
+import com.spring.boot.entities.Contact;
 import com.spring.boot.entities.projections.ContactSummary;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ContactService {
 
     List<ContactSummary> getAll();
+
+    ContactSummary getById(UUID id);
+
+    void save(Contact contact);
+
+    void update(Contact contact);
+
+    void deleteById(UUID id);
 }
