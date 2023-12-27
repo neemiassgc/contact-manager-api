ca41b78948820e518a77304b13705eab205f5f80
Neemias Santos <neemiassgc@outlook.com>
2023-12-26 21:44:49 -0300

    Create a new successful test method named should_return_all_the_contacts_for_a_given_user

--- src/test/java/com/spring/boot/services/ContactServiceIT.java
+++ src/test/java/com/spring/boot/services/ContactServiceIT.java
@@ -1,14 +1,31 @@
 package com.spring.boot.services;
 
+import com.spring.boot.entities.Contact;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
+@Transactional
 public class ContactServiceIT {
 
     @Autowired
     private ContactService contactService;
+
+    @Test
+    void should_return_all_the_contacts_for_a_given_user() {
+        final UUID robertUserId = UUID.fromString("773d20b6-bbf1-4c10-b743-5e7b693ef3ee");
+        final List<Contact> listOfContacts = contactService.findAllByUserId(robertUserId);
+
+        assertThat(listOfContacts).hasSize(4);
+        assertThat(listOfContacts).extracting(Contact::getName)
+            .containsExactly("Best friend Julia", "Mom", "Pizza and burgers", "Uncle Jeff");
+    }
 
 
     @Test
