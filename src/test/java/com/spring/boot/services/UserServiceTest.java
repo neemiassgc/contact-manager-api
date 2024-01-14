2e2963e2a33141b055063cf880f7d0fda09c14d0
Neemias Santos <neemiassgc@outlook.com>
2024-01-13 22:48:15 -0300

    new: Create a new test class named UserServiceTest with two tests written

---
+++ src/test/java/com/spring/boot/services/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.spring.boot.services;
+
+import com.spring.boot.entities.User;
+import static org.assertj.core.api.Assertions.*;
+
+import com.spring.boot.repositories.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void should_return_a_user_by_its_username() {
+        final User user = userService.findByUsername("robert");
+
+        assertThat(user).isNotNull();
+        assertThat(user).extracting("username").isEqualTo("robert");
+    }
+
+    @Test
+    void should_throw_an_error_when_user_does_not_exist() {
+        final Throwable throwable = catchThrowable(() -> userService.findByUsername("fred"));
+
+        assertThat(throwable).isNotNull();
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(((ResponseStatusException) throwable).getReason()).isEqualTo("User not found");
+        assertThat(((ResponseStatusException) throwable).getStatusCode()).isEqualTo(HttpStatusCode.valueOf(404));
+    }
+}
