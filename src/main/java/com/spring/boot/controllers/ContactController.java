6d9734205ff928d8c0ab81f955df73f1cb74163d
Neemias Santos <neemiassgc@outlook.com>
2024-01-13 22:53:39 -0300

    refactor: Make ContactManagerService be autowired by constructor by annotating it with @RequiredAgsConstructor

--- src/main/java/com/spring/boot/controllers/ContactController.java
+++ src/main/java/com/spring/boot/controllers/ContactController.java
@@ -2,25 +2,24 @@ package com.spring.boot.controllers;
 
 import com.spring.boot.entities.Contact;
 import com.spring.boot.entities.projections.ContactSummary;
-import com.spring.boot.repositories.ContactRepository;
-import com.spring.boot.services.ContactService;
+import com.spring.boot.services.ContactManagerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ContactController {
 
-    @Autowired
-    private ContactService contactService;
+    private final ContactManagerService contactManagerService;
 
     @GetMapping("/contacts")
     public List<ContactSummary> getAllContacts() {
-        return contactService.getAll();
+        return Contact.toListOfContactSummary(contactManagerService.findAll());
     }
 }
