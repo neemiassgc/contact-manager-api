5a19b2bba164be0b8a35f74eed811f4e822dd6bb
Neemias Santos <neemiassgc@outlook.com>
2024-01-16 00:46:04 -0300

    new: Create the endpoint /api/contacts/{id} to get a contact by its id

--- src/main/java/com/spring/boot/controllers/ContactController.java
+++ src/main/java/com/spring/boot/controllers/ContactController.java
@@ -6,10 +6,12 @@ import com.spring.boot.services.ContactManagerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -21,5 +23,10 @@ public class ContactController {
     @GetMapping("/contacts")
     public List<ContactSummary> getAllContacts() {
         return Contact.toListOfContactSummary(contactManagerService.findAll());
+    }
+
+    @GetMapping("/contacts/{id}")
+    public ContactSummary getById(@PathVariable("id") UUID id) {
+        return contactManagerService.findById(id).toContactSummary();
     }
 }
