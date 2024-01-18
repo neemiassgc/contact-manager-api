3463556bd9b39b45feffd81c51d1ee023fe54b56
Neemias Santos <neemiassgc@outlook.com>
2024-01-18 11:12:24 -0300

    new: Create a new query method named existsByIdAndUser implemented with custom JPQL

--- src/main/java/com/spring/boot/repositories/ContactRepository.java
+++ src/main/java/com/spring/boot/repositories/ContactRepository.java
@@ -13,4 +13,7 @@ public interface ContactRepository extends org.springframework.data.repository.R
 
     @Query("select c from Contact c join fetch c.phoneNumberMap p join fetch c.emailMap e join fetch addressMap a where c.id = :id")
     Optional<Contact> findById(@Param("id") UUID id);
+
+    @Query("select count(c.name) = 1 from Contact c where c.id = :id and c.user.username = :username")
+    boolean existsByIdAndUser(@Param("id") UUID id, @Param("username") String username);
 }
