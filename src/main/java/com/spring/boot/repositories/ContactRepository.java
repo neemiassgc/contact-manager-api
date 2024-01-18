878a90ab087280c4fbbdafe16a2365a928702196
Neemias Santos <neemiassgc@outlook.com>
2024-01-18 11:27:43 -0300

    refactor: Remove the existsByIdAndUser query method from ContactRepository

--- src/main/java/com/spring/boot/repositories/ContactRepository.java
+++ src/main/java/com/spring/boot/repositories/ContactRepository.java
@@ -11,9 +11,6 @@ import java.util.UUID;
 @Repository
 public interface ContactRepository extends org.springframework.data.repository.Repository<Contact, UUID>, ExtendedContactRepository {
 
-    @Query("select c from Contact c join fetch c.phoneNumberMap p join fetch c.emailMap e join fetch addressMap a where c.id = :id")
+    @Query("select c from Contact c join fetch c.phoneNumberMap p join fetch c.emailMap e join fetch addressMap a join fetcg c.user u where c.id = :id")
     Optional<Contact> findById(@Param("id") UUID id);
-
-    @Query("select count(c.name) = 1 from Contact c where c.id = :id and c.user.username = :username")
-    boolean existsByIdAndUser(@Param("id") UUID id, @Param("username") String username);
 }
