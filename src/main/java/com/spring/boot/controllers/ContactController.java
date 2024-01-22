5225e25b7611af74259781a2423a720e9ee32875
Neemias Santos <neemiassgc@outlook.com>
2024-01-21 23:29:21 -0300

    new: Create new a controller method that permits to create new contacts through POST /api/contacts

--- src/main/java/com/spring/boot/controllers/ContactController.java
+++ src/main/java/com/spring/boot/controllers/ContactController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -34,5 +31,11 @@ public class ContactController {
     public ContactSummary getById(@PathVariable("id") UUID id, @AuthenticationPrincipal Jwt jwt) {
         final String currentUser = jwt.getClaimAsString("username");
         return contactManagerService.findByIdWithUser(id, currentUser).toContactSummary();
+    }
+
+    @PostMapping("/contacts")
+    public void create(@RequestBody ContactSummary contactSummary, @AuthenticationPrincipal Jwt jwt) {
+        final String currentUser = jwt.getClaimAsString("username");
+        contactManagerService.saveWithUser(Contact.toContact(contactSummary), currentUser);
     }
 }
