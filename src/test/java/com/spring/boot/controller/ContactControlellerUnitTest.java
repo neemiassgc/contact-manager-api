7721ca193d1bf68d74094727d721134a14b99439
Neemias Santos <neemiassgc@outlook.com>
2024-01-25 23:54:58 -0300

    test: Create a initialization method named init annotated with @BeforeEach

--- src/test/java/com/spring/boot/controller/ContactControlellerUnitTest.java
+++ src/test/java/com/spring/boot/controller/ContactControlellerUnitTest.java
@@ -1,15 +1,28 @@
 package com.spring.boot.controller;
 
+import com.spring.boot.controllers.ContactController;
+import com.spring.boot.controllers.GlobalErrorController;
 import com.spring.boot.services.ContactManagerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@WebMvcTest(value = {ContactController.class, GlobalErrorController.class})
 public class ContactControlellerUnitTest {
 
     @MockBean
     private ContactManagerService contactManagerService;
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders
+            .standaloneSetup(new ContactController(contactManagerService), new GlobalErrorController())
+            .alwaysDo(MockMvcResultHandlers.print())
+            .build();
+    }
 }
