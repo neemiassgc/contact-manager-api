be64b8a98485e7b58034cd91e360d1f01a4873de
Neemias Santos <neemiassgc@outlook.com>
2024-01-28 02:49:32 -0300

    refactor: configure MockMvc by annotation and fix the test should_response_all_the_contacts_for_Joe_with_OK_200 making it pass

--- src/test/java/com/spring/boot/controller/ContactControlellerUnitTest.java
+++ src/test/java/com/spring/boot/controller/ContactControlellerUnitTest.java
@@ -4,48 +4,40 @@ import com.spring.boot.TestResources;
 import com.spring.boot.controllers.ContactController;
 import com.spring.boot.controllers.GlobalErrorController;
 import com.spring.boot.services.ContactManagerService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(value = {ContactController.class, GlobalErrorController.class})
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
 public class ContactControlellerUnitTest {
 
     @MockBean
     private ContactManagerService contactManagerService;
 
+    @Autowired
     private MockMvc mockMvc;
-
-    @BeforeEach
-    public void init() {
-        mockMvc = MockMvcBuilders
-            .standaloneSetup(new ContactController(contactManagerService), new GlobalErrorController())
-            .alwaysDo(MockMvcResultHandlers.print())
-            .build();
-    }
 
     @Test
     @DisplayName("GET /api/contacts OK 200")
-    void should_response_all_the_contacts_with_OK_200() throws Exception {
-        when(contactManagerService.findAll()).thenReturn(TestResources.getAFewContacts(10));
+    void should_response_all_the_contacts_for_Joe_with_OK_200() throws Exception {
+        when(contactManagerService.findAllByUsername(eq("joe")))
+            .thenReturn(TestResources.getContactsForJoe());
 
         mockMvc.perform(
             get("/api/contacts")
-            .header("Authorization", "Bearer "+jwtToken())
+            .header("Authorization", "Bearer "+jwtTokenForJoe())
             .accept(MediaType.APPLICATION_JSON)
         )
         .andExpect(status().isOk())
@@ -55,6 +47,10 @@ public class ContactControlellerUnitTest {
             "+359(10)4094-9549",
             "+52(54)6536-5876",
             "+65(77)4248-0921")));
+
+        verify(contactManagerService, times(1)).findAllByUsername(eq("joe"));
+        verifyNoMoreInteractions(contactManagerService);
+    }
 
     private String jwtTokenForJoe() {
         return """
