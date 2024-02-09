9daa97d4741e702c916518bab60c79f0e0e3737d
Neemias Santos <neemiassgc@outlook.com>
2024-02-09 00:19:35 -0300

    refactor: Modify JPQL string to execute 'left join' instead of 'join'

--- src/main/java/com/spring/boot/repositories/ExtendedContactRepository.java
+++ src/main/java/com/spring/boot/repositories/ExtendedContactRepository.java
@@ -13,7 +13,7 @@ public interface ExtendedContactRepository {
     @Query("select c from Contact c join fetch c.phoneNumberMap join fetch c.emailMap join fetch addressMap join fetch c.user")
     List<Contact> findAll();
 
-    @Query("select c from Contact c join fetch c.phoneNumberMap join fetch c.emailMap join fetch addressMap where c.user.username = :username")
+    @Query("select c from Contact c left join fetch c.phoneNumberMap left join fetch c.emailMap left join fetch addressMap where c.user.username = :username")
     List<Contact> findAllByUsername(@Param("username") String username);
 
     void deleteAll();
