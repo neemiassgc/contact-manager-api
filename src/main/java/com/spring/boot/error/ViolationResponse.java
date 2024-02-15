2618728b6b0d5a8f5ca2b9cecb7d3db929b98850
Neemias Santos <neemiassgc@outlook.com>
2024-02-14 22:15:50 -0300

    new: Create a new object called ViolationResponse that will represent the error response for field violations

---
+++ src/main/java/com/spring/boot/error/ViolationResponse.java
@@ -0,0 +1,21 @@
+package com.spring.boot.error;
+
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ViolationResponse {
+
+    @Getter
+    private final List<String> fieldViolations = new ArrayList<>();
+
+    public void putFieldViolation(final String violation) {
+       fieldViolations.add(violation);
+    }
+
+    public void putFieldViolation(final FieldError fieldError) {
+        putFieldViolation(fieldError.getDefaultMessage());
+    }
+}
