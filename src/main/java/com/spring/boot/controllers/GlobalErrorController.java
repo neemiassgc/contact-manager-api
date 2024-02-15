d541a8256daa195b5992a057b94f90634b24b178
Neemias Santos <neemiassgc@outlook.com>
2024-02-14 22:19:35 -0300

    new: Create a new exception handler method in charge of converting MethodArgumentNotValidException exceptionsinto Http error responses

--- src/main/java/com/spring/boot/controllers/GlobalErrorController.java
+++ src/main/java/com/spring/boot/controllers/GlobalErrorController.java
@@ -1,10 +1,11 @@
 package com.spring.boot.controllers;
 
+import com.spring.boot.error.ViolationResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Objects;
@@ -18,5 +19,15 @@ public class GlobalErrorController {
             .status(rse.getStatusCode())
             .contentType(MediaType.TEXT_PLAIN)
             .body(Objects.requireNonNullElseGet(rse.getReason(), () -> "No reasons"));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ViolationResponse> resolveResponseStatusException(final MethodArgumentNotValidException manve) {
+        final ViolationResponse violationResponse = manve
+            .getFieldErrors()
+            .stream()
+            .collect(ViolationResponse::new, ViolationResponse::putFieldViolation, (a, b) -> {});
+
+        return ResponseEntity.badRequest().body(violationResponse);
     }
 }
