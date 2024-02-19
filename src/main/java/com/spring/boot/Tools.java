f78ed63315b8b5a2ba1d6c23350e71f174078602
Neemias Santos <neemiassgc@outlook.com>
2024-02-19 00:41:25 -0300

    new: Create a class that will include useful functions called Tools and a helper function named immutableMap

---
+++ src/main/java/com/spring/boot/Tools.java
@@ -0,0 +1,14 @@
+package com.spring.boot;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+public final class Tools {
+
+    private Tools() {}
+
+    public static <L, R> Map<L, R> immutableMap(final Map<L, R> map) {
+        return Objects.isNull(map) ? null : Collections.unmodifiableMap(map);
+    }
+}
