275f72311cebd2b7fb50226ee9fdc80bedf8b80a
Neemias Santos <neemiassgc@outlook.com>
2024-03-27 20:57:30 -0300

    new: Add Jakarta validations on the SimpleUser's fields

--- src/main/java/com/spring/boot/entities/projections/SimpleUser.java
+++ src/main/java/com/spring/boot/entities/projections/SimpleUser.java
@@ -1,5 +1,8 @@
 package com.spring.boot.entities.projections;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +10,14 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public final class SimpleUser {
 
+    @Size(min = 3, max = 20, message = "'username' must be between 3 and 20 long")
+    @NotNull(message = "'username' must not be null")
     private final String username;
+
+    @Pattern(
+        regexp = "^(https?)://[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}(/[/a-zA-Z0-9._-]+)*$",
+        message = "'avatarUri' must be a valid Url"
+    )
     private final String avatarUri;
 
     public SimpleUser(final String username) {
