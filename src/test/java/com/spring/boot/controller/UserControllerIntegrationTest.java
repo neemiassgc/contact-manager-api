7b51d17e4b29b0960969388c3a21ae8f7c14953e
Neemias Santos <neemiassgc@outlook.com>
2024-03-28 23:24:08 -0300

    testing: Test if an error response with feedback is returned when Posting an invalid Json

--- src/test/java/com/spring/boot/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/spring/boot/controller/UserControllerIntegrationTest.java
@@ -9,8 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
@@ -30,5 +31,23 @@ public class UserControllerIntegrationTest {
             .header("Authorization", "Bearer "+ TestResources.jwtTokenWithAdminAuthority())
         )
         .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("POST /api/users -> 400 BAD REQUEST")
+    public void should_respond_400_with_feedback_when_sending_an_invalid_json() throws Exception {
+        final String jsonBody = "{\"avatarUri\": \"ftp://example.com/my-avatar.png\"}";
+        mockMvc.perform(post("/api/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.ALL)
+            .content(jsonBody)
+            .header("Authorization", "Bearer "+TestResources.jwtTokenWithAdminAuthority())
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(
+            jsonPath("$.fieldViolations[*]",
+            containsInAnyOrder("'avatarUri' must be a valid Url", "'username' must not be null"))
+        );
     }
 }
