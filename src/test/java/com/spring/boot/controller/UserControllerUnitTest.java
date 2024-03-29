bc54466b5146d2f319b08d1faa1b5432e6ebe2ec
Neemias Santos <neemiassgc@outlook.com>
2024-03-28 21:57:14 -0300

    testing: Create a unit test to check if the user creation at /api/users endpoint is sucessful

--- src/test/java/com/spring/boot/controller/UserControllerUnitTest.java
+++ src/test/java/com/spring/boot/controller/UserControllerUnitTest.java
@@ -1,13 +1,23 @@
 package com.spring.boot.controller;
 
+import com.spring.boot.TestResources;
 import com.spring.boot.controllers.GlobalErrorController;
 import com.spring.boot.controllers.UserController;
+import com.spring.boot.entities.User;
 import com.spring.boot.services.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static com.spring.boot.TestResources.once;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = {UserController.class, GlobalErrorController.class})
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
@@ -18,4 +28,22 @@ public class UserControllerUnitTest {
 
     @MockBean
     private UserService userService;
+
+    @Test
+    @DisplayName("POST /api/users -> 201 CREATED")
+    public void should_create_a_new_user_successfully() throws Exception {
+        doNothing().when(userService).create(any(User.class));
+
+        final String jsonBody = "{\"username\": \"julia\", \"avatarUri\": \"https://example.com/my-avatar.png\"}";
+        mockMvc.perform(post("/api/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.ALL)
+            .content(jsonBody)
+            .header("Authorization", "Bearer "+ TestResources.jwtTokenWithAdminAuthority())
+        )
+        .andExpect(status().isCreated());
+
+        verify(userService, once()).create(any(User.class));
+        verifyNoMoreInteractions(userService);
+    }
 }
