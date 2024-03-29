9e81867c0f1ff54c30e42a357502f209fcc73869
Neemias Santos <neemiassgc@outlook.com>
2024-03-28 23:20:16 -0300

    testing: Create an integration test to valid the sucessful creation of a user

--- src/test/java/com/spring/boot/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/spring/boot/controller/UserControllerIntegrationTest.java
@@ -1,9 +1,16 @@
 package com.spring.boot.controller;
 
+import com.spring.boot.TestResources;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
@@ -11,4 +18,17 @@ public class UserControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("POST /api/users -> 201 CREATED")
+    public void should_create_a_new_user_successfully() throws Exception {
+        final String jsonBody = "{\"username\": \"julia\", \"avatarUri\": \"https://example.com/my-avatar.png\"}";
+        mockMvc.perform(post("/api/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.ALL)
+            .content(jsonBody)
+            .header("Authorization", "Bearer "+ TestResources.jwtTokenWithAdminAuthority())
+        )
+        .andExpect(status().isCreated());
+    }
 }
