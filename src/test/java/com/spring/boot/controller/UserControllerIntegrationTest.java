6e4802edd407e14b98284b16ec241acb975ffc25
Neemias Santos <neemiassgc@outlook.com>
2024-03-28 23:28:34 -0300

    testing: Test an unsuccessful creation of a user that already exists through an integration test

--- src/test/java/com/spring/boot/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/spring/boot/controller/UserControllerIntegrationTest.java
@@ -1,15 +1,21 @@
 package com.spring.boot.controller;
 
 import com.spring.boot.TestResources;
+import com.spring.boot.entities.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
+import static com.spring.boot.TestResources.once;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -49,5 +55,20 @@ public class UserControllerIntegrationTest {
             jsonPath("$.fieldViolations[*]",
             containsInAnyOrder("'avatarUri' must be a valid Url", "'username' must not be null"))
         );
+    }
+
+    @Test
+    @DisplayName("POST /api/users -> 400 BAD REQUEST")
+    public void should_respond_400_when_trying_to_create_a_user_that_already_exists() throws Exception {
+        final String jsonBody = "{\"username\": \"joe\", \"avatarUri\": \"https://example.com/my-avatar.png\"}";
+        mockMvc.perform(post("/api/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.ALL)
+            .content(jsonBody)
+            .header("Authorization", "Bearer "+TestResources.jwtTokenWithAdminAuthority())
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+        .andExpect(content().string("User already exists"));
     }
 }
