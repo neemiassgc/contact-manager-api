7f8a0f5164529cf36c376ac2a9d68320fd5955a7
Neemias Santos <neemiassgc@outlook.com>
2024-03-31 23:16:41 -0300

    new: Create a class to implement a custom naming strategy for tables in the DB'

---
+++ src/main/java/com/spring/boot/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,33 @@
+package com.spring.boot;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public final class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier logicalName, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier logicalName, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier logicalName, JdbcEnvironment jdbcEnvironment) {
+        return Identifier.toIdentifier("cm_"+logicalName.getText());
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier logicalName, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier logicalName, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+}
