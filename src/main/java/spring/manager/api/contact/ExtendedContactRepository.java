cd52db392576db6fd8ca8cc6aa34e205c31a42a7
Neemias Santos <neemiassgc@outlook.com>
2024-06-15 00:58:20 -0300

    fix: use reference to the id of the User entity on JPA query string

--- src/main/java/spring/manager/api/contact/ExtendedContactRepository.java
+++ src/main/java/spring/manager/api/contact/ExtendedContactRepository.java
@@ -12,7 +12,7 @@ public interface ExtendedContactRepository {
     @Query("select c from Contact c join fetch c.phoneNumberMap join fetch c.emailMap join fetch addressMap join fetch c.user")
     List<Contact> findAll();
 
-    @Query("select c from Contact c left join fetch c.phoneNumberMap left join fetch c.emailMap left join fetch addressMap where c.user.user_id = :userId")
+    @Query("select c from Contact c left join fetch c.phoneNumberMap left join fetch c.emailMap left join fetch addressMap where c.user.id = :userId")
     List<Contact> findAllByUserId(@Param("userId") String userId);
 
     void deleteAll();
