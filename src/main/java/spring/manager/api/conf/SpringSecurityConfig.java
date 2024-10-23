f8f56226c0eb0ac5a4785aaa75f071a12a04a297
Neemias Santos <neemiassgc@outlook.com>
2024-10-23 11:06:27 -0300

    build: create a controller for handling warmup requests named WarmupHandler and apply security on it

--- src/main/java/spring/manager/api/conf/SpringSecurityConfig.java
+++ src/main/java/spring/manager/api/conf/SpringSecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +19,20 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(AbstractHttpConfigurer::disable);
-        httpSecurity.authorizeHttpRequests(it -> it.requestMatchers(HttpMethod.POST, "/api/users").authenticated());
-        httpSecurity.authorizeHttpRequests(it -> it.requestMatchers(HttpMethod.GET, "/warmup").permitAll());
-        httpSecurity.authorizeHttpRequests(it -> it.anyRequest().authenticated());
+        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/api/users").authenticated());
+        httpSecurity.authorizeHttpRequests(auth ->
+            auth.requestMatchers(
+                new AndRequestMatcher(
+                    new RequestHeaderRequestMatcher("X-Appengine-Cron", "True"),
+                    new AntPathRequestMatcher("/warmup-db", HttpMethod.GET.name())
+                )
+            ).permitAll());
+        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/warmup").permitAll());
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
         httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
-        httpSecurity.oauth2ResourceServer(it -> it.jwt(Customizer.withDefaults()));
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(AbstractHttpConfigurer::disable);
+        httpSecurity.oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
 
         return httpSecurity.build();
     }
