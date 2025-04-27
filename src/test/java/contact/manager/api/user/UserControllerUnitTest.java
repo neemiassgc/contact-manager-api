d34aed71fa14cbf773597f8bb4fa39e2b70d5938
Neemias Santos <neemiassgc@outlook.com>
2025-04-27 19:32:32 -0300

    test: rearrage the tests by grouping them by method for better clarity

--- src/test/java/contact/manager/api/user/UserControllerUnitTest.java
+++ src/test/java/contact/manager/api/user/UserControllerUnitTest.java
@@ -2,6 +2,7 @@ package contact.manager.api.user;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,11 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
 import contact.manager.api.global.GlobalErrorController;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,68 +33,112 @@ public class UserControllerUnitTest {
     @MockBean
     private UserService userService;
 
-    @Test
-    @DisplayName("POST /api/users -> 201 CREATED")
-    public void should_create_a_new_user_successfully() throws Exception {
-        doNothing().when(userService).create(any(User.class));
+    private final Jwt DEFAULT_JWT = Users.JOE.jwt();
 
-        final String jsonBody = "{\"username\": \"julia\"}";
-        mockMvc.perform(
-            post("/api/users").with(
-                SecurityMockMvcRequestPostProcessors.jwt().jwt(Users.JULIA.jwt())
+    @Nested
+    public class Create {
+
+        @Test
+        @DisplayName("Should create a new user successfully")
+        public void shouldCreateANewUserSuccessfully() throws Exception {
+            String newUserJson = "{\"username\": \"julia\"}";
+            doNothing().when(userService).create(any(User.class));
+
+            mockMvc.perform(
+                post("/api/users").with(
+                    SecurityMockMvcRequestPostProcessors.jwt().jwt(Users.JULIA.jwt())
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .content(newUserJson)
             )
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.ALL)
-            .content(jsonBody)
-        )
-        .andExpect(status().isCreated());
+            .andExpect(status().isCreated());
 
-        verify(userService, once()).create(any(User.class));
-        verifyNoMoreInteractions(userService);
-    }
+            verify(userService, once()).create(any(User.class));
+        }
 
-    @Test
-    @DisplayName("POST /api/users -> 400 BAD REQUEST")
-    public void should_respond_400_with_feedback_when_sending_an_invalid_json() throws Exception {
-        final String jsonBody = "{}";
-        mockMvc.perform(
-            post("/api/users").with(
-                SecurityMockMvcRequestPostProcessors.jwt().jwt(Users.ROBERT.jwt())
+
+        @Test
+        @DisplayName("When a user already exists then should respond 400")
+        public void whenAUserAlreadyExists_thenShouldRespond400() throws Exception {
+            doThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists"))
+                .when(userService).create(any(User.class));
+
+            final String jsonBody = "{\"username\": \"joe\"}";
+            mockMvc.perform(
+                post("/api/users").with(
+                    SecurityMockMvcRequestPostProcessors.jwt().jwt(DEFAULT_JWT)
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .content(jsonBody)
             )
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.ALL)
-            .content(jsonBody)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(
-            jsonPath("$.fieldViolations[*][*]",
-            Matchers.contains("username is required"))
-        );
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+            .andExpect(content().string("User already exists"));
 
-        verifyNoInteractions(userService);
-    }
+            verify(userService, once()).create(any(User.class));
+        }
 
-    @Test
-    @DisplayName("POST /api/users -> 400 BAD REQUEST")
-    public void should_respond_400_when_trying_to_create_a_user_that_already_exists() throws Exception {
-        doThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists"))
-            .when(userService).create(any(User.class));
-
-        final String jsonBody = "{\"username\": \"joe\"}";
-        mockMvc.perform(
-            post("/api/users").with(
-                SecurityMockMvcRequestPostProcessors.jwt().jwt(Users.JOE.jwt())
+        @Test
+        @DisplayName("When there is no body in request body then should respond 400")
+        public void whenThereIsNoBodyInRequest_thenShouldRespond400() throws Exception {
+            final String jsonBodyWithoutUsername = "";
+            mockMvc.perform(
+                post("/api/users").with(
+                    SecurityMockMvcRequestPostProcessors.jwt().jwt(DEFAULT_JWT)
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .content(jsonBodyWithoutUsername)
             )
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.ALL)
-            .content(jsonBody)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType(MediaType.TEXT_PLAIN))
-        .andExpect(content().string("User already exists"));
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+            .andExpect(content().string(Matchers.containsString("Required request body is missing")));
 
-        verify(userService, once()).create(any(User.class));
-        verifyNoMoreInteractions(userService);
+            verifyNoInteractions(userService);
+        }
+
+        @Test
+        @DisplayName("When provided a malformed then should respond 400")
+        public void whenProvidedAMalformedJson_thenShouldRespond400() throws Exception {
+            final String malformedJson = "{\"username\":}";
+            mockMvc.perform(
+                post("/api/users").with(
+                    SecurityMockMvcRequestPostProcessors.jwt().jwt(DEFAULT_JWT)
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .content(malformedJson)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+            .andExpect(content().string(Matchers.containsString("JSON parse error")));
+
+            verifyNoInteractions(userService);
+        }
+
+        @Test
+        @DisplayName("When provided an invalid username then should respond 400 along with feedback")
+        public void whenProvidedAnInvalidUsername_thenShouldRespond400AlongWithFeedback() throws Exception {
+            final String invalidUsername = "{\"username\":\"@-094kja+=el!a'd;url*\"}";
+            mockMvc.perform(
+                post("/api/users").with(
+                    SecurityMockMvcRequestPostProcessors.jwt().jwt(DEFAULT_JWT)
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .content(invalidUsername)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.fieldViolations.value[*]", hasSize(2)))
+            .andExpect(jsonPath("$.fieldViolations.value[*]", containsInAnyOrder(
+                "username needs to be between 3 and 20 characters long",
+                "username must not have special characters"
+            )));
+
+            verifyNoInteractions(userService);
+        }
     }
 }
