727a5b9ff32de07e24b6e86c84f5390481a770f3
Neemias Santos <neemiassgc@outlook.com>
2025-04-27 19:28:32 -0300

    chore(Username): add regex pattern validation for username field

--- src/main/java/contact/manager/api/user/Username.java
+++ src/main/java/contact/manager/api/user/Username.java
@@ -2,6 +2,7 @@ package contact.manager.api.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 public final class Username {
 
     @Size(min = 3, max = 20, message = "username needs to be between 3 and 20 characters long")
+    @Pattern(regexp = "[a-zA-z0-9.@]{3,}", message = "username must not have special characters")
     @NotNull(message = "username is required")
     @JsonProperty("username")
     private String value;
